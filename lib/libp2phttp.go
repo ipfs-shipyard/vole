83c956311ffc005b7a7c86a5a76fda32a333bd32
Marco Munizaga <git@marcopolo.io>
2024-04-04 16:47:20 -0700

    Add https support

--- lib/libp2phttp.go
+++ lib/libp2phttp.go
@@ -2,9 +2,18 @@ package vole
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"math/big"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"time"
 
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
@@ -32,6 +41,25 @@ func Libp2pHTTPSocketProxy(ctx context.Context, p multiaddr.Multiaddr, unixSocke
 		ai.ID = id
 	}
 
+	hasTLS := false
+	hasHTTP := false
+	multiaddr.ForEach(p, func(c multiaddr.Component) bool {
+		if c.Protocol().Code == multiaddr.P_HTTP {
+			hasHTTP = true
+		}
+
+		if c.Protocol().Code == multiaddr.P_HTTPS {
+			hasHTTP = true
+			hasTLS = true
+			return false
+		}
+
+		if c.Protocol().Code == multiaddr.P_TLS {
+			hasTLS = true
+		}
+		return true
+	})
+
 	rt, err := httpHost.NewConstrainedRoundTripper(ai)
 	if err != nil {
 		return err
@@ -56,6 +84,20 @@ func Libp2pHTTPSocketProxy(ctx context.Context, p multiaddr.Multiaddr, unixSocke
 		server.Close()
 	}()
 
+	if hasTLS && hasHTTP {
+		c, err := selfSignedTLSConfig()
+		if err != nil {
+
+			return err
+		}
+		server.TLSConfig = c
+
+		fmt.Println("Endpoint is an HTTPS endpoint. Using a self signed cert locally to proxy.")
+		fmt.Println("Curl will only work with -k flag. This is only for debugging. Do *not* use this in production.")
+
+		return server.ServeTLS(l, "", "")
+	}
+
 	return server.Serve(l)
 }
 
@@ -68,4 +110,47 @@ func Libp2pHTTPServer() (host.Host, *libp2phttp.Host, error) {
 
 	httpHost := &libp2phttp.Host{StreamHost: h}
 	return h, httpHost, nil
+}
+
+func selfSignedTLSConfig() (*tls.Config, error) {
+	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * time.Hour)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, err
+	}
+
+	certTemplate := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"Test"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, &certTemplate, &priv.PublicKey, priv)
+	if err != nil {
+		return nil, err
+	}
+
+	cert := tls.Certificate{
+		Certificate: [][]byte{derBytes},
+		PrivateKey:  priv,
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+	return tlsConfig, nil
 }
